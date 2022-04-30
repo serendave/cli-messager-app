7c0966cf644a96fe9ca8d122af1f612417405c1c
Denis <1denrocks1@gmail.com>
2022-04-30 15:07:53 +0300

    added logic of sending message for discord bot

--- apps/discord.go
+++ apps/discord.go
@@ -1,11 +1,99 @@
 package apps
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 func Discord() {
 	token := os.Getenv("DISCORD_TOKEN")
-	fmt.Println(token)
+	addBotUrl := os.Getenv("DISCORD_BOT_URL")
+
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	dg.Identify.Intents = discordgo.IntentGuildMembers | discordgo.IntentsGuildMessages | discordgo.IntentsGuilds
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection", err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	fmt.Println("Add bot to your server via this link: " + addBotUrl)
+
+	dg.AddHandler(bogJoined)
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+}
+
+func bogJoined(s *discordgo.Session, m *discordgo.GuildCreate) {
+	members, _ := s.GuildMembers(m.Guild.ID, "", 100)
+	reader := bufio.NewReader(os.Stdin)
+
+	var selectedMember discordgo.Member
+
+	fmt.Println("Discord bot is added to your server. \nSelect which user you want to write")
+
+	for i, member := range members {
+		if member.User.ID != s.State.User.ID {
+			fmt.Printf("%d: %s\n", i+1, member.User.Username)
+		}
+	}
+
+	for {
+		fmt.Print("Type the number: ")
+		char, _, err := reader.ReadRune()
+
+		if err != nil {
+			panic(err)
+		}
+
+		number := int(char - '0')
+
+		if number >= len(members) {
+			fmt.Println("Please, provide a valid number")
+		} else {
+			selectedMember = *members[number-1]
+			break
+		}
+	}
+
+	channel, err := s.UserChannelCreate(selectedMember.User.ID)
+
+	if err != nil {
+		panic(err)
+	}
+
+	message, err := reader.ReadString('\n')
+
+	for {
+		fmt.Println("Type message you want to send to this user: ")
+		message, err = reader.ReadString('\n')
+
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = s.ChannelMessageSend(channel.ID, message)
+
+		if err != nil {
+			panic(err)
+		} else {
+			fmt.Println("Message sent successfully")
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/serendave/cli-messager-app
 
 go 1.18
 
-require github.com/joho/godotenv v1.4.0 // indirect
+require (
+	github.com/bwmarrin/discordgo v0.25.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+)
